c8c75d89d54bbf23884d8fbb27f87458e116e3be
joaquimserafim <joaquim.serafim@gmail.com>
2014-04-08 15:37:47 +0100

    Fix the database initialization on views/load

--- views/load.js
+++ views/load.js
@@ -4,9 +4,29 @@ var async = require('async');
 var views = ['dequeue', 'queue', 'complete', 'queues'];
 var db = nano.use(process.env.DB || 'cloudq');
 
-var start = module.exports = function (cb) { async.map(views, load, cb); };
 
-function load(view, done) {
+function initDB (cb) {
+  nano.db.get(process.env.DB || 'cloudq', function (err) {
+    if(err && err.message === 'no_db_file') {
+      return nano.db.create(process.env.DB || 'cloudq', cb);
+    }
+    return cb();
+  });
+}
+
+// exports module
+var start = module.exports = load;
+
+
+function load (cb) {
+  initDB(function (err) {
+    if (err) return cb(err);
+    // load views
+    async.map(views, setViews, cb);
+  });
+}
+
+function setViews (view, done) {
   db.get('_design/' + view, function (err, body) {
     var data = require('./' + view);
     if (err) { return db.insert(data, '_design/' + view, done); }
@@ -16,22 +36,10 @@ function load(view, done) {
 }
 
 
-function initDB (done) {
-  nano.db.get(process.env.DB || 'cloudq', function (err) {
-    if(err && err.message === 'no_db_file') {
-      nano.db.create(process.env.DB || 'cloudq', done);
-    }
-    done();
-  });
-}
-
-
 if (!module.parent) {
-  initDB(function () {
-    start(function (err, res) {
-      if (err) { console.log(err); }
-      console.log(res);
-      process.exit(0);
-    });
+  start(function (err, res) {
+    if (err) { console.log(err); }
+    console.log(res);
+    process.exit(0);
   });
 }
