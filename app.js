22ac99faaf955c91b982a24105f7bc430dcf62a5
joaquimserafim <joaquim.serafim@gmail.com>
2014-04-18 01:27:34 +0100

    middleware finished

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
 if (process.env.NEWRELIC_KEY) { require('newrelic'); }
 var _ = require('underscore');
-var moment = require('moment');
-
-var Middleware = require('./middleware');
-
 var express = require('express');
-var log = require('./logger');
-var TIMEOUT = process.env.TIMEOUT || 500;
-var SUCCESS = 200;
-var ERROR = 500;
 
+var log = require('./logger');
+var Middleware = require('./middleware');
 // Basic Auth - for now, in v3 implement user/queue based auth
 var auth = require('./lib/auth')(process.env.TOKEN, process.env.SECRET);
 
-var agentkeepalive = require('agentkeepalive');
-var myagent = new agentkeepalive({
-  maxSockets: 50,
-  maxKeepAliveRequests: 0,
-  maxKeepAliveTime: 30000
-});
-
-var nano = require('nano')({
-  url: process.env.COUCH || 'http://localhost:5984',
-  request_defaults: { agent: myagent }
-});
-
-var db = nano.use(process.env.DB || 'cloudq');
+var TIMEOUT = process.env.TIMEOUT || 500;
 
 var app = express();
 var mid = new Middleware();
-
 var workers = {};
 
-// TODO: User API
+
+//
+
+function logger () {
+  return function (req, res, next) {
+    var _start = new Date();
+
+    function logRequest () {
+      log.info({req: req, res: res});
+      log.info('Exec Time', (new Date()) - _start, 'ms');
+    }
+
+    res.once('finish', logRequest);
+    res.once('close', logRequest);
+    next();
+  };
+}
+
+function respError (err, code, res) {
+  log.error(err);
+  res.send(code, {error: err.message});
+}
+
+/// EXPRESS
+
+module.exports = app;
+
+// Express configuration
 
 app.configure('development', function () {
   app.use(logger());
@@ -46,111 +54,24 @@ app.configure(function () {
   app.use(express.json());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-
 });
-
-
-function respError (err, res) {
-  log.error(err);
-  res.send(500, err.message);
-}
-
-// TODO: User API
-
-// Cloudq API - ROUTES
-
-// return stats
-app.get('/stats', function (req, res) {
-  mid.stats(function (err, stats) {
-    if (err) return respError(err, res);
-    res.send(SUCCESS, stats);
-  });
-});
-
-// publish job
-app.post('/:queue', auth, publish);
-app.put('/:queue', auth, publish);
-
-// consume job - update state to Processing
-app.get('/:queue', auth, function (req, res) {
-  mid.consume(req.params.queue, function (err, msg) {
-    if (err) return respError(err, res);
-
-    if (msg) return res.send(SUCCESS, msg);
-
-    // queue worker instead of returning response
-    if (!workers[req.params.queue]) workers[req.params.queue] = [];
-
-    workers[req.params.queue].push(res);
-
-    // listen for timeout
-    setTimeout(function() {
-      res.send(SUCCESS, {status: 'empty'});
-      // dequeue worker...
-      workers[req.params.queue] = _(workers[req.params.queue]).without(res);
-    }, TIMEOUT);
-
-  });
-});
-
-// delete job - update state to Completed
-app.del('/:queue/:id', auth, function (req, res) {
-  mid.completed(req.params.id, function (err, msg) {
-    if (err) return respError(err, res);
-    res.send(msg);
-  });
-});
-
-module.exports = app;
 
 // lib
-function logger () {
-  return function (req, res, next) {
-    var _start = new Date();
-
-    function logRequest () {
-      log.info({req: req, res: res});
-      log.info('Exec Time', (new Date()) - _start, 'ms');
-    }
-
-    res.once('finish', logRequest);
-    res.once  ('close', function () {
-      delete workers[req.params.queue];
-      logRequest();
-    });
-
-    next();
-  };
-}
 
 function publish (req, res) {
-  if (!req.body) {
-    log.error('could not find body');
-    return res.send(ERROR, {error: 'must submit a job'});
-  }
-  var o = req.body;
-  if (!o.job) {
-    log.error('could not find job');
-    return res.send(ERROR, {error: 'job not found!'});
-  }
-  _.extend(o, {
-    type: req.params.queue,
-    state: 'published',
-    publishedAt: new Date(),
-    expires_in: moment().add('days', 2),
-    priority: o.priority || 100
-  });
+  // check content-type, only application/json
+  if (!req.is('application/json'))
+    return respError(new Error('the content type must be "application/json"'), 500, res);// code 415
 
-  db.insert(o, function (err, body) {
-    if (err) {
-      log.error(err);
-      return res.send(500, err);
-    }
+  if (!req.body || !req.body.job)
+    return respError(new Error('must submit a valid job'), 500, res);// code 400
 
-    res.send(SUCCESS, body);
-    o._id = body.id;
-    // could emit event for job added if changes queue doesn't work
-    notify(o);
+  mid.publish(req.body, req.params.queue, function (err, doc) {
+    if (err) return respError(err, 500, res);
+    // response to client
+    res.send(doc);
+    // if worker is listening - notify..
+    notify(_.extend(req.body.job, {id: doc.id, type: req.params.queue}));
   });
 }
 
@@ -160,18 +81,69 @@ function notify (doc) {
   // find queue, find worker...
   if (_.isArray(workers[doc.type]) && !_.isEmpty(workers[doc.type])) {
     var wkr = workers[doc.type].shift();
-    // update doc as processing
-    db.atomic('dequeue', 'id', doc._id, function (err, body) {
-      if (err) {
-        log.error(err);
-        return wkr.send(ERROR, err);
-      }
 
-      var job = _.extend(doc.job, {
-        id: doc._id,
-        ok: true
-      });
-      wkr.send(SUCCESS, job);
+    // update doc as processing
+    mid.dequeue(doc.id, function (err, res) {
+      if (err) return respError(err, 500, wkr);
+
+      delete doc.type;
+      doc.ok = res;
+      wkr.send(doc);
     });
   }
 }
+
+
+// Cloudq API - ROUTES
+
+// return stats
+app.get('/stats', function (req, res) {
+  mid.stats(function (err, stats) {
+    if (err) return respError(err, 500, res);
+    res.send(stats);
+  });
+});
+
+// publish job
+app.post('/:queue', auth, publish);
+app.put('/:queue', auth, publish);
+
+// consume job - update state to Processing
+app.get('/:queue', auth, function (req, res) {
+  mid.consume(req.params.queue, function (err, doc) {
+    if (err) return respError(err, 500, res);
+
+    if (doc) return res.send(doc);
+
+    // queue worker instead of returning response
+    if (!workers[req.params.queue]) workers[req.params.queue] = [];
+
+    workers[req.params.queue].push(res);
+
+    function dequeueResponse () {
+      workers[req.params.queue] = _(workers[req.params.queue]).without(res);
+    }
+
+    var responseTimeoutId = setTimeout(function () {
+      log.info({req: req}, 'Queue request timeout');
+      dequeueResponse();
+      res.send({status: 'empty'});
+    }, TIMEOUT);
+
+    res.once('close', function () {
+      log.info({req: req}, 'Queue request terminated');
+      clearTimeout(responseTimeoutId);
+      dequeueResponse();
+    });
+
+  });
+});
+
+// delete job - update state to Completed
+app.del('/:queue/:id', auth, function (req, res) {
+  mid.complete(req.params.id, function (err, doc) {
+    if (err) return respError(err, 500, res);// code 400
+
+    res.send(doc);
+  });
+});
--- middleware.js
+++ middleware.js
@@ -1,90 +1,113 @@
 var _ = require('underscore');
-
 var agentkeepalive = require('agentkeepalive');
 var nano = require('nano')({
   url: process.env.COUCH || 'http://localhost:5984',
   request_defaults: {
-      agent: new agentkeepalive({
-      maxSockets: 50,
-      maxKeepAliveRequests: 0,
-      maxKeepAliveTime: 30000
-    })
-  }
+    agent: new agentkeepalive({
+    maxSockets: 50,
+    maxKeepAliveRequests: 0,
+    maxKeepAliveTime: 30000
+  })}
 });
-
 
 
 module.exports = Middleware;
 
 function Middleware () {
   if (!(this instanceof Middleware)) return new Middleware();
-  this.db = nano.use(process.env.DB || 'cloudq');
+  this._db = nano.use(process.env.DB || 'cloudq');
 }
 
-Middleware.prototype.statify = function (rows) {
-  return _(rows).chain().filter(function (row) {
-     return _(row.key).has('state');
-   }).map(function (row) {
-    return {
-      type: row.key.type,
-      state: row.key.state,
-      value: row.value
-    };
-  })
-  .groupBy('type')
-  .map(function (v, k) {
-    var _value = {};
-    _(v).each(function (r) {
-      _value[r.state] = r.value;
-    });
-    return {key: k, value: _value};
-  })
-  .value();
-};
+
+Middleware.prototype.dequeue = function (id, cb) {
+  this._db.atomic('dequeue', 'id', id, function (err, body) {// must review body res
+    if (err) return cb(err);
+
+    cb(null, true);
+  });
+}
+
 
 Middleware.prototype.stats = function (cb) {
   var self = this;
-  self.db.view('queues', 'all', {group: true, reduce: true}, function (err, body) {
+  self._db.view('queues', 'all', {group: true, reduce: true}, function (err, body) {
     if (err) return cb(err);
 
-    var stats = self.statify(body.rows);
+    if (!body.rows.length) return cb(null, []);
+
+    var stats = _(body.rows)
+    .chain()
+    .filter(function (row) {
+       return _(row.key).has('state');
+     })
+    .map(function (row) {
+      return {
+        type: row.key.type,
+        state: row.key.state,
+        value: row.value
+      };
+    })
+    .groupBy('type')
+    .map(function (v, k) {
+      var _value = {};
+      _(v).each(function (r) {
+        _value[r.state] = r.value;
+      });
+      return {key: k, value: _value};
+    })
+    .value();
+
     cb(null, stats);
   });
 };
 
-Middleware.prototype.publish = function (obj, cb) {
-  this._db.insert(obj, cb);
+
+Middleware.prototype.publish = function (obj, queue, cb) {
+  var now = new Date();
+
+  _.extend(obj, {
+    type: queue,
+    state: 'published',
+    publishedAt: now,
+    expires_in: new Date(now.setDate(new Date().getDate() + 2)),
+    priority: obj.priority || 100
+  });
+
+  this._db.insert(obj, function (err, body) {
+    if (err) return cb(err);
+
+    cb(null, body);
+  });
 };
 
-Middleware.prototype.completed = function (id, cb) {
-  this.db.atomic('complete', 'id', id, function (err, body) {
+
+Middleware.prototype.complete = function (id, cb) {
+  this._db.atomic('complete', 'id', id, function (err, body) {
     if (err) return cb(err);
 
     cb(null, {status: body});
   });
 };
 
+
 Middleware.prototype.consume = function (queue, cb) {
   var self = this;
 
-  self.db.view('queue', 'next', {
+  // search for jobs
+  self._db.view('queue', 'next', {
     startkey: [queue, 1],
     endkey: [queue, 100],
     limit: 1
   }, function (err, body) {
     if (err) return cb(err);
-    // now jobs, return the cb
+    // no jobs, return the cb
     if (!body.rows.length) return cb(null, null);
-
-    // jobs
+    // consume job
     var doc = body.rows[0];
-    self.db.atomic('dequeue', 'id', doc.id, function (_err) {
+    self.dequeue(doc.id, function (_err, _body) {
       if (_err) return cb(_err);
 
-      doc.value.id = doc.id;
-      doc.value.ok = true;
-
-      cb(null, doc.value);
+      cb(null, _.extend(doc.value, {id: doc.id, ok: _body}));
     });
   });
 };
